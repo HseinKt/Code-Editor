0aaae1afbbbaa82b0cb4a66e17856f7bf44135c8
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-25 15:10:08 +0200

    Added a login and register buttons to the header component.

--- code_editor/src/components/header.js
+++ code_editor/src/components/header.js
@@ -5,6 +5,10 @@ const header =()=>{
         <div><h3>Logo Goes here</h3></div>
         <div><h1>Editor Name</h1></div>
       </div>
+      <div>
+        <div><button>Login</button></div>
+        <div><button>Register</button></div>
+      </div>
     </header>
   )
 }
