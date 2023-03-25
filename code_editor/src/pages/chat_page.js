f9bc001d4464856277111ab855be91d89a076edf
HseinKt <hseinkteish1998@gmail.com>
2023-03-25 23:30:09 +0200

    created the input text and the button submit

--- code_editor/src/pages/chat_page.js
+++ code_editor/src/pages/chat_page.js
@@ -1,12 +1,36 @@
 import Header from "../components/landing_page/header"
 import Chat from "../components/chat_page/chat"
+import { useState } from "react";
 
 const ChatingPage = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState("");
+
+    const handleValue = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleMessageSend = (e) => {
+        e.preventDefault();
+        setMessages([...messages, value]);
+        setValue("");
+
+    }
+    console.log(messages);
+
     return ( 
         <div>
             <Header/>
-            <h1>chat page</h1>
-            <Chat />
+            <form onSubmit={handleMessageSend}>
+                <input 
+                    type="text" 
+                    value={value} 
+                    onChange={handleValue}
+                />
+                <button type="submit">
+                    <div className="sendButton"> send</div>
+                </button>
+            </form>
         </div>
      );
 }
