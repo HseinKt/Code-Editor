f46c320086fb8748ec3645e071036300aa5155f6
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-25 12:24:57 +0200

    added users_table component

--- code_editor/src/App.js
+++ code_editor/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import users_table from "./components/admin_panel/users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <users_table />
     </div>
   );
 }
