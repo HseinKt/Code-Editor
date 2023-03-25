58974ad70840478eabdf0b48a321c65482863fde
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-25 17:13:11 +0200

    Added a footer component for the landing page.

---
+++ code_editor/src/components/landing_page/footer.js
@@ -0,0 +1,14 @@
+const footer = ()=>{
+  return(
+    <div className="footer">
+      <h3>Editor Name</h3>
+      <p>Copyright &#169; 2023 Team 5</p>
+      <div>
+        <p>Legal Stuff</p>
+        <p>Privacy Policy</p>
+        <p>Security</p>
+      </div>
+    </div>
+  )
+}
+export default footer
