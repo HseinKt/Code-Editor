e847cec557539d9dc7daed87d51d035c95597c6f
AlINJFSw <alijanbeincs@gmail.com>
2023-03-25 17:49:09 +0200

    added console

--- code_editor/src/pages/coding_page.js
+++ code_editor/src/pages/coding_page.js
@@ -1,32 +1,67 @@
 import AceEditor from "react-ace";
-
 import "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/mode-python";
 
 import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-tomorrow_night_blue";
 import { useState } from "react";
 import CodingHeader from "../components/coding_panel/coding_header";
 const CodingPage = () => {
-    const [code,setCode] = useState('')
+  const [showFiles, setShowFiles] = useState(true);
+  const [code, setCode] = useState('');
+  const toggleShowMyFiles = () => {
+    console.log(showFiles);
+    setShowFiles(!showFiles);
+  };
+
+  const saveHandler = () => {
+    console.log("save");
+  };
+  const runHandler = () => {
+    console.log("run");
+  };
+  const codeHandler = (value) => {
+    setCode(value)
+    console.log(value);
+  }
+
   return (
-    <div>
-        <CodingHeader/>
-      <AceEditor
-        placeholder="Start Coding... "
-        mode="python"
-        theme="monokai"
-        fontSize={20}
-        showPrintMargin={true}
-        showGutter={true}
-        highlightActiveLine={true}
-        value = {code}
-        setOptions={{
-          showLineNumbers: true,
-          tabSize: 4,
-        }}
+    <>
+      <CodingHeader
+        onSave={saveHandler}
+        onshowfiles={toggleShowMyFiles}
+        onRun={runHandler}
       />
-      ;
-    </div>
+      <div className="code">
+        <AceEditor
+          placeholder="Start Coding... "
+          mode="python"
+          width="100%"
+          theme="tomorrow_night_blue"
+          fontSize={20}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          value={code}
+          onChange = {codeHandler}
+          setOptions={{
+            showLineNumbers: true,
+            tabSize: 4,
+          }}
+        />
+        <div className="console">
+          {showFiles ? <div>
+            alionj
+          </div> :  <div>
+          <label>
+            {" "}
+            {`->`}output :<br />
+          </label>
+          <label> </label>
+          </div>}
+        </div>
+      </div>
+    </>
   );
 };
 export default CodingPage;
