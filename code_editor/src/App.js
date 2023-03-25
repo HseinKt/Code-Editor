1f30d83d6aec5d5bcc917228b96c650346658d1e
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-25 14:50:08 +0200

    Added landing page

--- code_editor/src/App.js
+++ code_editor/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import CodingPage from "./pages/coding-page";
+import landingPage from "./pages/landing_page";
+
 function App() {
   return (
     <>
       <Routes>
         {/* <Route path="/" element = {}/> */}
-        <Route path="/" element={<CodingPage />} />
+        <Route path="/coding_page" element={<CodingPage />} />
+        <Route path="/" element={<landingPage />} />
+
       </Routes>
     </>
   );
---
+++ code_editor/src/pages/landing_page.js
@@ -0,0 +1,11 @@
+const landingPage = () => {
+  return (
+    <div>
+      <h1>
+          Hussein Turfah
+      </h1>
+    </div>
+  )
+}
+
+export default landingPage
