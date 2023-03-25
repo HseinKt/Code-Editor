14d1673b4185fe849becc6a54857d57e6f702682
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-25 15:06:03 +0200

    Added a header component for the landing page.

---
+++ code_editor/src/components/header.js
@@ -0,0 +1,12 @@
+const header =()=>{
+  return(
+    <header className="header">
+      <div>
+        <div><h3>Logo Goes here</h3></div>
+        <div><h1>Editor Name</h1></div>
+      </div>
+    </header>
+  )
+}
+
+export default header
--- code_editor/src/pages/landing_page.js
+++ code_editor/src/pages/landing_page.js
@@ -1,16 +1,9 @@
-const LandingPage = () => {
+import Header from "../components/header"
+
+const landingPage = () => {
   return (
-    <header className="header">
-      <div>
-        <div><h3>Logo Goes here</h3></div>
-        <div><h1>Editor Name</h1></div>
-      </div>
-      <div>
-        <div><button>Login</button></div>
-        <div><button>Register</button></div>
-      </div>
-    </header>
+    <Header />
   )
 }
 
-export default LandingPage
+export default landingPage
