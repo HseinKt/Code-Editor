99e3aa5708fb681c77ae557755163595c6b48c75
HseinKt <hseinkteish1998@gmail.com>
2023-03-25 20:41:59 +0200

    updated on the the search page

--- code_editor/src/components/search_page/card.js
+++ code_editor/src/components/search_page/card.js
@@ -1,18 +1,13 @@
-import React, {useState} from 'react'
 import "./search.css";
 
-const Card = () => {
-
-    const [name, setName] = useState("Hsein kteish");
-    const [major, setMajor] = useState("Engineer");
-
+const Card = (props) => {
     return ( 
         <div className='container'>
             <div className="card_container">
-                <div className="card_image"></div>
+                <div className="card_image"> </div>
                 <div className='details'>
-                    <h4 className="name"><b>{name}</b></h4>
-                    <p className="major">{major}</p>
+                    <h4 className="name"><b>{props.name}</b></h4>
+                    <p className="major">{props.major}</p>
                 </div>
             </div>
         </div>
