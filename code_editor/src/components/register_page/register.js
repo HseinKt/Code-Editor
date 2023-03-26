20a8f19fd616b0c96329d55d8a5e43c5b0d1cad3
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-26 03:21:38 +0300

    Fixed some stylings in the registration page

--- code_editor/src/components/register_page/register.js
+++ code_editor/src/components/register_page/register.js
@@ -4,42 +4,45 @@ const register = ()=>{
       <div className="register-container">
         <h1 className="register-title">Register</h1>
         <div className="register-form">
+        <div className="name-container">
+            <div>
+              <label htmlFor="first_name">First Name</label>
+              <input type="text" id="first_name" placeholder="First Name" />
+            </div>
+            <div>
+              <label htmlFor="last_name">Last Name</label>
+              <input type="text" id="last_name" placeholder="Last Name" />
+            </div>
+          </div>
           <div>
             <label htmlFor="email">Enter your Email</label>
             <input id="email" placeholder="Email" />
           </div>
           <div>
-            <label htmlFor="username">Choose a username</label>
+            <label htmlFor="username">Choose a Username</label>
             <input id="username" placeholder="Username" />
           </div>
           <div>
-            <label htmlFor="password">Enter your password</label>
+            <label htmlFor="password">Enter your Password</label>
             <input id="password" type="password" placeholder="Password" />
           </div>
           <div>
-            <label htmlFor="confirm_password">Confirm your password</label>
+            <label htmlFor="confirm_password">Confirm your Password</label>
             <input id="confirm_password" type="password" placeholder="Confirm Password" />
-          </div>
-          <div className="name-container">
-            <div className="input-wrapper">
-              <label htmlFor="first_name">First Name</label>
-              <input type="text" id="first_name" placeholder="First Name" />
             </div>
-            <div className="input-wrapper">
-              <label htmlFor="last_name">Last Name</label>
-              <input className="x" type="text" id="last_name" placeholder="Last Name" />
-            </div>
-          </div>
           <div>
-            <label htmlFor="birth_date">Enter your birth_date</label>
+            <label htmlFor="birth_date">Date of Birth</label>
             <input type="date" id="birth_date"/>
           </div>
-          <label htmlFor="gender">Choose your gender</label>
           <div id="gender" className="gender">
-            <label htmlFor="male">Male</label>
-            <input type="radio" id="male" value="male" />
+            <div>
+              <label htmlFor="male">Male</label>
+              <input type="radio" id="male" value="male" />
+            </div>
+            <div>
             <label htmlFor="female">Female</label>
             <input type="radio" id="female" value="female" />
+            </div>
           </div>
           <button type="submit" className="register-button" id="register-button">Register</button>
           <div className="register-link">
