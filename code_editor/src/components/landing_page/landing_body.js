7bb33c4c0820c13253fe500e205e456ce69a2f29
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-26 15:36:19 +0300

    Added several elements to the landing page.

--- code_editor/src/components/landing_page/landing_body.js
+++ code_editor/src/components/landing_page/landing_body.js
@@ -1,8 +1,39 @@
+import Compilerimage from "../images/compiler.png"
+
 const landing_body=()=>{
   return(
-    <h1>
-      Compile your code with ease using our Python compiler
-    </h1>
+    <div className="landing_body">
+      <div className="landing_row">
+        <div>
+          <h2>
+            Compile your code with ease using our Python compiler
+          </h2>
+          <h5>
+            Instant IDE
+          </h5>
+          <h4>
+            Code right from your browser
+          </h4>
+          <p>
+            Codeit is the best tool for starting, sharing, and developing
+            projects in Python programming language, right from your
+            browser
+          </p>
+          <button>Start Coding</button>
+        </div>
+        <div>
+          <img src={Compilerimage} height="200px"></img>
+        </div>
+      </div>
+      <div className="landing_row">
+        <div>
+          Hello World!
+        </div>
+        <div>
+        Hello World!
+        </div>
+      </div>
+    </div>
   )
 }
 export default landing_body
