297f826c8a11e089698b8b2997d074171c03528c
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-26 19:16:19 +0300

    Added registration api.

--- code_editor_server/app/Http/Controllers/RegisterController.php
+++ code_editor_server/app/Http/Controllers/RegisterController.php
@@ -6,7 +6,53 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 use App\Http\Controllers\FunctionsController;
+use Illuminate\Support\Facades\Auth;
+
 class RegisterController extends Controller
 {
-    //
+    function register(Request $request){
+        $functions_controller = new FunctionsController();
+
+        $username = $functions_controller -> entryValidate($request ->username);
+        $password = $functions_controller -> entryValidate($request ->password);
+
+        $first_name = $functions_controller -> entryValidate($request ->first_name);
+        $last_name = $functions_controller -> entryValidate($request ->last_name);
+        $gender = $functions_controller -> entryValidate($request ->gender);
+
+        $salt = $functions_controller -> generateRandomString(4);
+        $hashed_password = hash('sha256',$password.$salt);
+
+        $date = now()->format('d-m-y');
+        $gender = ucfirst(strtolower($gender));
+        
+        $verified_username = User::where('username', $username)->first();
+
+        if(!$verified_username){
+            
+            $user = new User;
+            $user->username = $username;
+            $user->password = $hashed_password;
+            $user->salt = $salt;
+            
+            $user->first_name = $first_name;
+            $user->last_name = $last_name;
+            $user->gender = $gender;
+            
+            $picture_encoded = $functions_controller->savePicture($request);
+            $user->picture = $picture_encoded;
+            $user->created_at = $date;
+    
+            $user->save();
+            
+            return response() -> json([
+                "user"=>$user
+            ]);
+
+        }else{
+            return response() -> json([
+                "error"=>"Username already exists!"
+            ]);
+        }
+    }
 }
