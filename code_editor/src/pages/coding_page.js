206f872ef6a987ce4dd3e9bbc99994c99ca70ee9
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-26 16:57:10 +0300

    added config giel

--- code_editor/src/pages/coding_page.js
+++ code_editor/src/pages/coding_page.js
@@ -10,8 +10,8 @@ import CodingHeader from "../components/coding_panel/coding_header";
 import MyFiles from "../components/coding_panel/my_files";
 const CodingPage = () => {
   const [showFiles, setShowFiles] = useState(true);
-  const [code, setCode] = useState('');
-  const [isSaving,setIsSaving] = useState(false);
+  const [code, setCode] = useState("");
+  const [isSaving, setIsSaving] = useState(false);
   const toggleShowMyFiles = () => {
     console.log(showFiles);
     setShowFiles(!showFiles);
@@ -21,30 +21,48 @@ const CodingPage = () => {
     setIsSaving(!isSaving);
     console.log(isSaving);
   };
-  const runHandler = () => {
-    console.log("run");
+  const runHandler = async () => {
+    // const data = {
+    //   clientId: "917f572690312065e575c94cd5444a8f",
+    //   clientSecret:
+    //     "ca8d0e47352dfb6b8ffb494523672c72b51e6463539d0ce40651bb84566eb24d",
+    //   script: "for i in range(5):\n  print('Hello World!')",
+    //   language: "python3",
+    //   versionIndex: "4",
+    // };
+    // const headers = {
+    //   "Content-Type": "application/json",
+    // };
+    // axios
+    //   .post("https://api.jdoodle.com/v1/execute", data, { headers })
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   };
   const savingclassNameHandler = () => {
     console.log("savingclassNameHandler");
-    setIsSaving(false)
+    setIsSaving(false);
   };
   const codeHandler = (value) => {
-    setCode(value)
+    setCode(value);
     console.log(value);
-  }
+  };
 
   const fileNameChangleHandler = (e) => {
     console.log(e.target.value);
-  }
+  };
   return (
     <>
       <CodingHeader
         onSave={saveHandler}
         onshowfiles={toggleShowMyFiles}
         onRun={runHandler}
-        onSaveClassName = {savingclassNameHandler}
-        isSaving = {isSaving}
-        onChange = {fileNameChangleHandler}
+        onSaveClassName={savingclassNameHandler}
+        isSaving={isSaving}
+        onChange={fileNameChangleHandler}
       />
       <div className="code">
         <AceEditor
@@ -58,20 +76,24 @@ const CodingPage = () => {
           showGutter={true}
           highlightActiveLine={true}
           value={code}
-          onChange = {codeHandler}
+          onChange={codeHandler}
           setOptions={{
             showLineNumbers: true,
             tabSize: 4,
           }}
         />
         <div className="console">
-          {showFiles ? <MyFiles/> :  <div>
-          <label>
-            {" "}
-            {`->`}output :<br />
-          </label>
-          <label> </label>
-          </div>}
+          {showFiles ? (
+            <MyFiles />
+          ) : (
+            <div>
+              <label>
+                {" "}
+                {`->`}output :<br />
+              </label>
+              <label> </label>
+            </div>
+          )}
         </div>
       </div>
     </>
