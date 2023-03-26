05bd1f426263a3d0c178f9ab08f577c83162356d
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-26 10:20:47 +0300

    Added a several elements for the login component

--- code_editor/src/components/login_page/login.js
+++ code_editor/src/components/login_page/login.js
@@ -1,6 +1,22 @@
 const login = ()=>{
   return(
-    <div>Login Page</div>
+    <div class="container">
+      <div class="login-container">
+        <h1 class="login-title">Login</h1>
+        <div class="login-form">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="Enter your Email"/>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter your Username"/>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your Password"/>
+          <button type="submit" class="login-button" id="login-button">Login</button>
+          <p class="forgot-password"><a href="#">Forgot your password?</a></p>
+          <p class="register-link">Not registered? <a href="register.html">Register as a new User</a>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default login
