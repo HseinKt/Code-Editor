34b8665b81547db53b0a1aca390dd7d71c85806c
HseinKt <hseinkteish1998@gmail.com>
2023-03-27 18:00:04 +0300

    finished search page

--- code_editor/src/components/search_page/card.js
+++ code_editor/src/components/search_page/card.js
@@ -1,13 +1,14 @@
 import "./search.css";
 
 const Card = (props) => {
+
     return ( 
         <div className='container'>
             <div className="card_container">
                 <div className="card_image"> </div>
                 <div className='details'>
-                    <h4 className="name"><b>{props.name}</b></h4>
-                    <p className="major">{props.gender}</p>
+                    <h4 className="name"><b>{props.data.first_name+" "+props.data.last_name}</b></h4>
+                    <p className="major">{props.data.gender}</p>
                 </div>
             </div>
         </div>
