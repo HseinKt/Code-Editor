dc49312846f5f5150039d1a6f710a744dc3f9799
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-28 02:58:02 +0300

    added double click to download

--- code_editor/src/components/coding_panel/file_name.js
+++ code_editor/src/components/coding_panel/file_name.js
@@ -1,6 +1,12 @@
 import UseHttp from "../../hooks/http-hook";
 
 const FileName = (props) => {
-  return <p onClick={() => props.onClick(props.name)}>{props.name}</p>;
+  return (
+    <p
+      onDoubleClick={() => props.onDoubleClick(props.name)}
+      onClick={() => props.onClick(props.name)}>
+      {props.name}
+    </p>
+  );
 };
 export default FileName;
