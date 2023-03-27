1062642e63ee797360fe18e030f197d00a36913d
Hussein Turfah <husseinturfah299@gmail.com>
2023-03-27 21:32:36 +0300

    Added deleteUsers function

--- code_editor_server/app/Http/Controllers/AdminController.php
+++ code_editor_server/app/Http/Controllers/AdminController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     function listUsers(){
-        $users = User::all("id","username","first_name","last_name");
+        $users = User::all("id","username","first_name","last_name","is_deleted");
         return json_encode($users);
+    }
+    function deleteUser(Request $request){
+        $id = $request -> id;
+        $user = User::find($id);
+        $user->is_deleted = 1;
+        return json_encode($user);
     }
 }
