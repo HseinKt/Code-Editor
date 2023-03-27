349ef5c456cc364195c3fd03f5e14f47b9f7be9b
Hussein Turfah <husseinturfah299@gmail.com>
2023-03-27 21:03:08 +0300

    Added AdminController with list users function

--- code_editor_server/app/Http/Controllers/AdminController.php
+++ code_editor_server/app/Http/Controllers/AdminController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     function listUsers(){
-        $users = User::all();
+        $users = User::all("id","username","first_name","last_name");
         return json_encode($users);
-
     }
 }
