573cce3d3578b13ae39d3683d4444dc39069b6a3
Ali-Ayb <aliayoub.me1@gmail.com>
2023-03-27 17:24:00 +0300

    added loop over the data taken from api in myfile components

--- code_editor/src/components/coding_panel/my_files.js
+++ code_editor/src/components/coding_panel/my_files.js
@@ -1,18 +1,22 @@
-import FileName from "./file_name"
+import FileName from "./file_name";
+import UseHttp from "../../hooks/http-hook";
+
+const token = localStorage.getItem("token");
+
+const data = await UseHttp("get_Files_Names", {
+  Authorization: "Bearer" + token,
+});
 
 const MyFiles = () => {
-    return(
-        <div className="my-files">
-            <h4>
-                My files
-            </h4>
-            <hr></hr>
-            <FileName name = {'start'} />
-            <FileName name = {'start'} />
-            <FileName name = {'start'} />
-            <FileName name = {'start'} />
-        </div>
-    )
-}
+  return (
+    <div className="my-files">
+      <h4>My files</h4>
+      <hr></hr>
+      {data.map((item) => (
+        <FileName key={item.id} name={item.name} />
+      ))}
+    </div>
+  );
+};
 
-export default MyFiles
+export default MyFiles;
