b26b602324038bfed326d13e6843da165dc5cfb1
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-27 03:04:06 +0300

    Added JS for handling user authentication

--- code_editor/src/components/login_page/login.js
+++ code_editor/src/components/login_page/login.js
@@ -1,21 +1,45 @@
-const login = ()=>{
+import { useRef } from "react";
+import UseHttp from "../../hooks/http-hook";
+import { useNavigate } from 'react-router-dom';
+
+
+const Login = ()=>{
+  const navigate = useNavigate();
+  const usernameRef=useRef("");
+  const passwordRef=useRef("");
+  
+  const saveToken = async () => {
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+      const formData = new FormData();
+      formData.append("username", username);
+      formData.append("password", password);
+      const data = await UseHttp(
+        "login",
+        "POST",
+        formData
+      );
+      localStorage.setItem('token', data.token);
+      navigate("/")
+    };
+    
+
   return(
-    <div class="lgn-container">
-      <div class="login-container">
-        <h1 class="login-title">Login</h1>
-        <div class="login-form">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="Enter your Email"/>
+    <div className="lgn-container">
+      <div className="login-container">
+        <h1 className="login-title">Login</h1>
+        <div className="login-form">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="Enter your Username"/>
+          <input type="text" id="username" placeholder="Enter your Username" ref={usernameRef}/>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Enter your Password"/>
-          <button type="submit" class="login-button" id="login-button">Login</button>
-          <p class="forgot-password"><a href="#">Forgot your password?</a></p>
-          <p class="register-link">Not registered? <a href="register.html">Register as a new User</a></p>
+          <input type="password" id="password" placeholder="Enter your Password" ref={passwordRef}/>
+          <button type="submit" className="login-button" id="login-button" onClick={saveToken} >Login</button>
+          <p className="forgot-password"><a href="#">Forgot your password?</a></p>
+          <p className="register-link">Not registered? <a href="register.html">Register as a new User</a></p>
         </div>
       </div>
     </div>
   )
 }
-export default login
+export default Login
