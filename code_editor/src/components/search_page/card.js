78eef189283d2537c7b99ac6d742a221b81e0edc
HseinKt <hseinkteish1998@gmail.com>
2023-03-28 14:20:33 +0300

    updatet on styling

--- code_editor/src/components/search_page/card.js
+++ code_editor/src/components/search_page/card.js
@@ -1,9 +1,9 @@
 import "./search.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import img from "../images/card.jpg"
 const Card = (props) => {
-    const { data, image } = props;
+    const { data, image } = props.image;
     const navigate = useNavigate();
     
     const handleSubmit = (event) => {
@@ -18,7 +18,7 @@ const Card = (props) => {
         <div className='container'>
             <div className="card_container">
                 <div className="card_image"> 
-                    <img className="image_card" src={image} alt="picture"/> 
+                    <img className="image_card" src={img} alt="picture"/> 
                 </div>
                 <div className='details'>
                     <h4 className="name"><b>{props.data.first_name+" "+props.data.last_name}</b></h4>
