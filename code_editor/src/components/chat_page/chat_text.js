70cb776cc327892ad42b6322fb9eefb5427f9de0
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-01 02:06:53 +0300

    fixed image in the search users

--- code_editor/src/components/chat_page/chat_text.js
+++ code_editor/src/components/chat_page/chat_text.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import "./chat.css"
+import React from "react";
+import "./chat.css";
 
 const ChatText = (props) => {
-    return (
-      <div className='chatTextContainer'>
-        <div className='message'>
-          <ul className='listStyle'>
-            <li className='name'><span className='targetName'>{props.target_name}</span>  </li>
-            <li className='sendMessage'>{props.message}</li>
-          </ul>
-        </div>
+  return (
+    <div className="chatTextContainer">
+      <div className="message">
+        <ul className="listStyle">
+          {/* <li className='name'><span className='targetName'>{props.target_name}</span>  </li> */}
+          <li className="sendMessage">{props.message}</li>
+        </ul>
       </div>
-    );
-  }
- 
+    </div>
+  );
+};
+
 export default ChatText;
-
-
-
--- code_editor/src/components/search_page/card.js
+++ code_editor/src/components/search_page/card.js
@@ -1,32 +1,35 @@
 import "./search.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import img from "../images/card.jpg"
 const Card = (props) => {
-    const { data, image } = props.image;
-    const navigate = useNavigate();
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        navigate(`/chat_page?id=${props.data.id}&name=${props.data.username}`);
-      };
+  const { data, image } = props;
+  const navigate = useNavigate();
 
-    // const imageData = `data:image/jpeg;base64,${props.data.picture}`;
-    console.log(props.data.picture);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/chat_page?id=${props.data.id}&name=${props.data.username}`);
+  };
 
-    return ( 
-        <div className='container'>
-            <div className="card_container">
-                <div className="card_image"> 
-                    <img className="image_card" src={img} alt="picture"/> 
-                </div>
-                <div className='details'>
-                    <h4 className="name"><b>{props.data.first_name+" "+props.data.last_name}</b></h4>
-                    <button className="messagebtn" onClick={handleSubmit}>send a message</button>
-                </div>
-            </div>
+  // const imageData = `data:image/jpeg;base64,${props.data.picture}`;
+  console.log(props.data.picture);
+
+  return (
+    <div className="container">
+      <div className="card_container">
+        <div className="card_image">
+          <img className="image_card" src={image} alt="picture" />
         </div>
-     );
-}
- 
+        <div className="details">
+          <h4 className="name">
+            <b>{props.data.first_name + " " + props.data.last_name}</b>
+          </h4>
+          <button className="messagebtn" onClick={handleSubmit}>
+            send a message
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Card;
